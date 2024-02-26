2bbaf1195b13f6576f2ba8d31a7cb9519fa0c024
Ian Vexler <ianvexler@Ians-MacBook-Pro.local>
2024-02-26 10:35:40 +0000

    Added tabs

--- example/pages/ListPage.tsx
+++ example/pages/ListPage.tsx
@@ -1,11 +1,10 @@
-import { Chip, List, ProgressBar, SearchBar, Title, Breadcrumb, FilterButton, Subtitle } from '@the-curve-consulting/texmo-react-components';
+import { Chip, List, ProgressBar, SearchBar, Title, Breadcrumb, FilterButton, Subtitle, Tabs } from '@the-curve-consulting/texmo-react-components';
 import { useEffect, useRef } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const ListPage = () => {
   const row1Ref = useRef<HTMLDivElement>(null!);
-  const row2Ref = useRef<HTMLDivElement>(null!);
 
   useEffect(() => {
     console.log(row1Ref.current.clientWidth);
@@ -13,6 +12,12 @@ const ListPage = () => {
 
   return (
     <div className="mt-5">
+
+      <Tabs>
+        <Tabs.Button text='Tab 1' selected/>
+        <Tabs.Button text='Tab 2'/>
+        <Tabs.Button text='Tab 3'/>
+      </Tabs>
 
     <Row className="mt-4">
       <Col>
@@ -50,77 +55,89 @@ const ListPage = () => {
         <List.Cell>Head 3</List.Cell>
         <List.Cell>Head 4</List.Cell>
         <List.Cell>Head 5</List.Cell>
+        <List.Cell link />
       </List.Head>
 
       <List.Body>
         <List.Row ref={row1Ref}>
-            <List.Col>
-                <List.Cell>
-                    <div className="mt-2">Test</div>
-                    <div>
-                        <h3>{'Test'}</h3>
-                    </div>
-                </List.Cell>
-            </List.Col>
+          <List.Cell>
+              <div className="mt-2">Test</div>
+              <div>
+                  <h3>{'Test'}</h3>
+              </div>
+          </List.Cell>
 
-            <List.Col>
-                <List.Cell borderStart>
-                    <Chip label="Test" theme="primary" />
-                </List.Cell>
-            </List.Col>
+          <List.Cell borderStart>
+              <Chip label="Test" theme="primary" />
+          </List.Cell>
 
-            <List.Col>
-                <List.Cell borderStart>
-                    <ProgressBar percentage={40} />
-                </List.Cell>
-            </List.Col>
+          <List.Cell borderStart>
+              <ProgressBar percentage={40} />
+          </List.Cell>
 
-            <List.Col>
-                <List.Cell borderStart>
-                    <p className="m-0">Test</p>
-                </List.Cell>
-            </List.Col>
+          <List.Cell borderStart>
+              <p className="m-0">Test</p>
+          </List.Cell>
 
-            <List.Col>
-                <List.Cell borderStart>
-                    <p className="m-0">Test</p>
-                </List.Cell>
-            </List.Col>
+          <List.Cell borderStart>
+              <p className="m-0">Test</p>
+          </List.Cell>
+
+          <List.Cell borderStart link />
         </List.Row>
 
-        <List.Row ref={row2Ref}>
-          <List.Col>
-              <List.Cell>
-                  <div className="mt-2">Test</div>
-                  <div>
-                      <h3>{`Test ${0}`}</h3>
-                  </div>
-              </List.Cell>
-          </List.Col>
+        <List.Row ref={row1Ref}>
+          <List.Cell>
+              <div className="mt-2">Test</div>
+              <div>
+                  <h3>{'Test'}</h3>
+              </div>
+          </List.Cell>
 
-          <List.Col>
-              <List.Cell borderStart>
-                  <Chip label="Test" theme="primary" />
-              </List.Cell>
-          </List.Col>
+          <List.Cell borderStart>
+              <Chip label="Test" theme="primary" />
+          </List.Cell>
 
-          <List.Col>
-              <List.Cell borderStart>
-                  <ProgressBar percentage={40} />
-              </List.Cell>
-          </List.Col>
+          <List.Cell borderStart>
+              <ProgressBar percentage={40} />
+          </List.Cell>
 
-          <List.Col>
-              <List.Cell borderStart>
-                  <p className="m-0">Test</p>
-              </List.Cell>
-          </List.Col>
+          <List.Cell borderStart>
+              <p className="m-0">Test</p>
+          </List.Cell>
 
-          <List.Col>
-              <List.Cell borderStart>
-                  <p className="m-0">Test</p>
-              </List.Cell>
-          </List.Col>
+          <List.Cell borderStart>
+              <p className="m-0">Test</p>
+          </List.Cell>
+
+          <List.Cell borderStart link />
+        </List.Row>
+
+        <List.Row ref={row1Ref}>
+          <List.Cell>
+              <div className="mt-2">Test</div>
+              <div>
+                  <h3>{'Test'}</h3>
+              </div>
+          </List.Cell>
+
+          <List.Cell borderStart>
+              <Chip label="Test" theme="primary" />
+          </List.Cell>
+
+          <List.Cell borderStart>
+              <ProgressBar percentage={40} />
+          </List.Cell>
+
+          <List.Cell borderStart>
+              <p className="m-0">Test</p>
+          </List.Cell>
+
+          <List.Cell borderStart>
+              <p className="m-0">Test</p>
+          </List.Cell>
+
+          <List.Cell borderStart link />
         </List.Row>
       </List.Body>
     </List>
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,6 @@
 import './index.scss';
 import { Router } from "./Router";
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 export default function App() {
   return (

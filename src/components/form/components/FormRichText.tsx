7aa8c00cafc4d72a60622046c89b0b476cb48cfe
Jamie Neighbours <jamie.neighbours@thecurve.io>
2024-06-18 14:48:19 +0100

    Update rich text component to use base quill instance

--- src/components/form/components/FormRichText.tsx
+++ src/components/form/components/FormRichText.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import Feedback from 'react-bootstrap/esm/Feedback';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  ForwardedRef, CSSProperties,
+} from 'react';
+import Quill from 'quill';
 
-export interface FormRichTextProps extends ReactQuill.ReactQuillProps {}
 
-const FormRichText = ({ style, ...rest }: FormRichTextProps) => {
-  return <ReactQuill theme="snow" style={{ ...style }} {...rest} />;
-};
+export interface QuillEditorProps{
+  className?: string,
+  style?: CSSProperties,
+  id?: string,
+  modules?: Record<string, unknown>,
+  onChange?: (value : string) => string;
+  value?: string,
+}
 
-FormRichText.Feedback = Feedback;
+const FormRichText: React.ForwardRefRenderFunction<Quill, QuillEditorProps> = ({modules, value, onChange, ...rest } :QuillEditorProps, ref: ForwardedRef<Quill>) => {
+  const editorRef = useRef<HTMLDivElement | null>(null);
+  const quillRef = useRef<Quill | null>(null);
 
-export default FormRichText;
+  useEffect(() => {
+    if (editorRef.current) {
+      const quill = new Quill(editorRef.current, modules);
+      quillRef.current = quill; // Store the Quill instance in a ref
+
+      if (ref) {
+        // Assign the Quill instance to the forwarded ref
+        if (typeof ref !== 'function') {
+          ref.current = quill; // For object refs
+        }
+
+        if (value) {
+          setValue(quill);
+        }
+        configureListeners(quill);
+      }
+    }
+  }, []);
+
+  useImperativeHandle(ref, () => quillRef.current as Quill);
+
+  const setValue = (quillRef: Quill) => {
+    const delta = quillRef.clipboard.convert({html: value})
+    quillRef.setContents(delta, 'silent')
+  }
+
+  const configureListeners = (quill: Quill) => {
+    quill.on('text-change', (e) => {
+      if (onChange) {
+        onChange(quillRef.current?.getSemanticHTML() || '');
+      }
+    });
+  }
+
+  return <div ref={editorRef} style={rest.style} id={rest.id}/>
+}
+
+
+export default forwardRef(FormRichText);
+
